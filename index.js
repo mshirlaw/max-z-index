1b31591305a2d8d96e6fa6d811fc92471235779f
Matt Shirlaw <matt.shirlaw@gmail.com>
2019-10-12 17:30:12 +1100

    Filter files that match z-index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/env node
 
-const finder = require('findit')(process.argv[2] || '.');
+const finder = require('findit')('.');
+const fs = require('fs');
 const path = require('path');
 
 const EXCLUDE_DIRS = ['node_modules', '.git'];
@@ -16,9 +17,20 @@ finder.on('directory', (directory, stat, stop) => {
 finder.on('file', (file, stat) => {
     VALID_EXTENSIONS.map(pattern => {
         if (file.match(pattern)) {
-            console.log(file);
+            process(file);
         }
     });
 });
 
+function process(file) {
+   const contents = fs.readFileSync(file, 'utf8');
+   const lines = contents.split('\n');
+   const matched = filter(lines);
+}
+
+function filter(lines) {
+   return lines
+    .map((line, index) => ({line, index}))
+    .filter(({line}) => line && line.match(/(z-index|zIndex)/)); 
+}
 
